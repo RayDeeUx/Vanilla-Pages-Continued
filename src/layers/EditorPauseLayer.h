7a2b4146dadad37f0c11883ec704952a9a743834
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2026-01-12 11:59:25 -0500

    burh omen

--- src/layers/EditorPauseLayer.h
+++ src/layers/EditorPauseLayer.h
@@ -19,8 +19,16 @@ class $modify(PageEditorPauseLayer, EditorPauseLayer) {
         if (Mod::get()->getSettingValue<bool>("editor-pause-guidelines-menu")) {
             if (auto guidelinesMenu = this->getChildByID("guidelines-menu")) {
                 guidelinesMenu->setLayout(RowLayout::create()->setAutoScale(true));
-                static_cast<PageMenu*>(guidelinesMenu)->setPaged(6, PageOrientation::HORIZONTAL, (CCDirector::get()->getWinSize().width / 2) - 35, 2);
+                static_cast<PageMenu*>(guidelinesMenu)->setPaged(7, PageOrientation::HORIZONTAL, (CCDirector::get()->getWinSize().width / 2) - 35);
                 static_cast<PageMenu*>(guidelinesMenu)->setButtonScale(.75f);
+            }
+        }
+
+        if (Mod::get()->getSettingValue<bool>("editor-pause-settings-menu")) {
+            if (auto settingsMenu = this->getChildByID("settings-menu")) {
+                settingsMenu->setLayout(RowLayout::create()->setAutoScale(true));
+                static_cast<PageMenu*>(settingsMenu)->setPaged(2, PageOrientation::HORIZONTAL, 35);
+                static_cast<PageMenu*>(settingsMenu)->setButtonScale(.5f);
             }
         }
 
